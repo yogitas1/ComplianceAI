1bb5476313fe68c47e58dbd29fbd214970acd4f3
Yogita <yogita.senthil@athenatec.com>
2026-06-06 13:07:29 -0700

    fixing demo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination: 'https://compliance-ai-three.vercel.app/api/:path*',
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig
